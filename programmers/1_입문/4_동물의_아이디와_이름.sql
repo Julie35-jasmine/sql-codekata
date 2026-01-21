e89b1c6085168b109da510c4b9f8fb9d82964ef1
DLPFC <jeul0854@gmail.com>
2026-01-21 10:19:19 +0900

    [프로그래머스] 입문 - 동물의 아이디와 이름 (#4)

--- programmers/1_입문/4_동물의_아이디와_이름.sql
+++ programmers/1_입문/4_동물의_아이디와_이름.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 입문 (⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59403
 -- 작성자: 이주은
--- 작성일: 2026. 01. 21. 10:19:05
+-- 작성일: 2026. 01. 21. 10:19:17
 
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME
