2320c5fce8ddc6528d5dbb0364486a268afce5cb
DLPFC <jeul0854@gmail.com>
2026-01-21 10:32:16 +0900

    [프로그래머스] 입문 - 모든 레코드 조회하기 (#47)

---
+++ programmers/1_입문/47_모든_레코드_조회하기.sql
@@ -0,0 +1,10 @@
+-- 모든 레코드 조회하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- 작성자: 이주은
+-- 작성일: 2026. 01. 21. 10:32:14
+
+-- 코드를 입력하세요
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
