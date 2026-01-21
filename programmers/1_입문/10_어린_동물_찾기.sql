6777a728da7e7d8b6166d2254ae9a25e00b2efb7
DLPFC <jeul0854@gmail.com>
2026-01-21 10:24:38 +0900

    [프로그래머스] 입문 - 어린 동물 찾기 (#10)

--- programmers/1_입문/10_어린_동물_찾기.sql
+++ programmers/1_입문/10_어린_동물_찾기.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 입문 (⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59037
 -- 작성자: 이주은
--- 작성일: 2026. 01. 21. 10:21:09
+-- 작성일: 2026. 01. 21. 10:24:37
 
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME
