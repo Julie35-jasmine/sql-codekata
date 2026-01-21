4709bdfccf2278c5a7487356faf8582211a5d6fa
DLPFC <jeul0854@gmail.com>
2026-01-21 10:20:46 +0900

    [프로그래머스] 입문 - 상위 n개 레코드 (#8)

---
+++ programmers/1_입문/8_상위_n개_레코드.sql
@@ -0,0 +1,11 @@
+-- 상위 n개 레코드
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- 작성자: 이주은
+-- 작성일: 2026. 01. 21. 10:20:44
+
+-- 코드를 입력하세요
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
