cdba18898ea05243276b6ade97dd4467d1c443e2
DLPFC <jeul0854@gmail.com>
2026-02-06 09:34:06 +0900

    [HackerRank] 입문 - Weather Observation Station 1 (#133)

---
+++ hackerrank/1_입문/133_Weather_Observation_Station_1.sql
@@ -0,0 +1,8 @@
+-- Weather Observation Station 1
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 06. 09:34:05
+
+SELECT CITY, STATE
+FROM STATION
