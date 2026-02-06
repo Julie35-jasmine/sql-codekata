15cb7008e051b9371fd16702e1f9654f615e6bd7
DLPFC <jeul0854@gmail.com>
2026-02-06 09:17:15 +0900

    [HackerRank] 입문 - Japanese Cities' Attributes (#131)

---
+++ hackerrank/1_입문/131_Japanese_Cities'_Attributes.sql
@@ -0,0 +1,9 @@
+-- Japanese Cities' Attributes
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 06. 09:17:12
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE='JPN'
