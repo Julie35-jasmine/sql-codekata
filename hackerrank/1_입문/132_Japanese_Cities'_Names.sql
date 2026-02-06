db64afbdf0efd89475fc4aa1e4546cf05a38284a
DLPFC <jeul0854@gmail.com>
2026-02-06 09:19:11 +0900

    [HackerRank] 입문 - Japanese Cities' Names (#132)

---
+++ hackerrank/1_입문/132_Japanese_Cities'_Names.sql
@@ -0,0 +1,9 @@
+-- Japanese Cities' Names
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 06. 09:19:08
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE='JPN'
