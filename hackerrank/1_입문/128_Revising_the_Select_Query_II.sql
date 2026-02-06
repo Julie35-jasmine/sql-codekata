99f5ab9843d288480279372117234a0e414f6fe3
DLPFC <jeul0854@gmail.com>
2026-02-06 09:07:43 +0900

    [HackerRank] 입문 - Revising the Select Query II (#128)

---
+++ hackerrank/1_입문/128_Revising_the_Select_Query_II.sql
@@ -0,0 +1,9 @@
+-- Revising the Select Query II
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 06. 09:07:41
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE='USA' AND POPULATION > 120000;
