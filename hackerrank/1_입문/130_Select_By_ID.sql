d279d42045379af9248b46326f91a8f054098fc2
DLPFC <jeul0854@gmail.com>
2026-02-06 09:15:04 +0900

    [HackerRank] 입문 - Select By ID (#130)

---
+++ hackerrank/1_입문/130_Select_By_ID.sql
@@ -0,0 +1,9 @@
+-- Select By ID
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/select-by-id/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 06. 09:15:01
+
+SELECT *
+FROM CITY
+WHERE ID=1661;
