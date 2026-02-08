0faf201650146a880eed94521d6e71aa06f1b9e9
DLPFC <jeul0854@gmail.com>
2026-02-08 18:54:19 +0900

    [HackerRank] 입문 - Employee Names (#146)

---
+++ hackerrank/1_입문/146_Employee_Names.sql
@@ -0,0 +1,9 @@
+-- Employee Names
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/name-of-employees/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 08. 18:54:18
+
+SELECT name
+FROM Employee
+ORDER BY name asc;
