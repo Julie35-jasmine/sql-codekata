a4aee3e9eb33e4d27d8bfd9d1105ab67f3868d33
DLPFC <jeul0854@gmail.com>
2026-02-11 09:30:00 +0900

    [HackerRank] 입문 - Average Population (#153)

---
+++ hackerrank/1_입문/153_Average_Population.sql
@@ -0,0 +1,8 @@
+-- Average Population
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/average-population/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 11. 09:29:59
+
+select round(avg(POPULATION), 0)
+from CITY
