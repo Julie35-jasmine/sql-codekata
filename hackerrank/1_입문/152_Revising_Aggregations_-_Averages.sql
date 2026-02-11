3854d0667fafb3e19d771c7c2b395d6bb2cb63c0
DLPFC <jeul0854@gmail.com>
2026-02-11 09:20:36 +0900

    [HackerRank] 입문 - Revising Aggregations - Averages (#152)

---
+++ hackerrank/1_입문/152_Revising_Aggregations_-_Averages.sql
@@ -0,0 +1,9 @@
+-- Revising Aggregations - Averages
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- 작성자: 이주은
+-- 작성일: 2026. 02. 11. 09:20:35
+
+SELECT avg(POPULATION)
+from CITY
+where DISTRICT = 'California'
