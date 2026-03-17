0a6ee84a2506fc5de0072a6c0d175a25788e4a3b
DLPFC <jeul0854@gmail.com>
2026-03-17 20:16:47 +0900

    [프로그래머스] 기초 - 카테고리 별 도서 판매량 집계하기 (#32)

--- programmers/2_기초/32_카테고리_별_도서_판매량_집계하기.sql
+++ programmers/2_기초/32_카테고리_별_도서_판매량_집계하기.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 기초 (⭐⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/144855
 -- 작성자: 이주은
--- 작성일: 2026. 03. 17. 20:16:13
+-- 작성일: 2026. 03. 17. 20:16:46
 
 -- 코드를 입력하세요
 SELECT B.CATEGORY, SUM(BS.SALES) AS TOTAL_SALES
